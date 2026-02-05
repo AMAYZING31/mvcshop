55cf5193d5a5147cce88aeed6d081cc9981fb999
sidourhs <sidneyrichards31@gmail.com>
2026-02-05 11:45:17 +0100

    Add product form with validation and update functionality

--- src/main/java/fr/iut/mvcshop/controller/ProduitController.java
+++ src/main/java/fr/iut/mvcshop/controller/ProduitController.java
@@ -1,19 +1,24 @@
 package fr.iut.mvcshop.controller;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import fr.iut.mvcshop.model.Produit;
 import fr.iut.mvcshop.repository.ProduitRepository;
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProduitController {
@@ -46,15 +51,60 @@ public class ProduitController {
         return "produits";
     }
 
-    @RequestMapping(value="/produitDelete", method=RequestMethod.GET)
-    public String supprimerProduit(Long id, int p, int s, String mc,RedirectAttributes redirectAttributes) {
+    @RequestMapping(value = "/produitDelete", method = RequestMethod.GET)
+    public String supprimerProduit(Long id, int p, int s, String mc, RedirectAttributes redirectAttributes) {
         // TODO on pourrait verifier si le produit existe avant
         this.repo.deleteById(id);
 
-        redirectAttributes.addAttribute("p",p);
-        redirectAttributes.addAttribute("s",s);
-        redirectAttributes.addAttribute("mc",mc);
+        redirectAttributes.addAttribute("p", p);
+        redirectAttributes.addAttribute("s", s);
+        redirectAttributes.addAttribute("mc", mc);
         return "redirect:/produits";
     }
-    
+
+    @GetMapping("/produitform")
+    public String editerProduits(
+            @RequestParam(value = "id", defaultValue = "0") Long id,
+            @RequestParam(value = "p", defaultValue = "0") int page,
+            @RequestParam(value = "s", defaultValue = "5") int size,
+            @RequestParam(value = "mc", defaultValue = "") String motCle,
+            Model model) {
+
+        if (id > 0) {
+            Optional<Produit> optPro = this.repo.findById(id);
+            if (optPro.isPresent()) {
+                model.addAttribute("produit", optPro.get());
+            } else {
+                return "redirect:/";
+            }
+        } else {
+            model.addAttribute("produit", new Produit());
+        }
+        model.addAttribute("p", page);
+        model.addAttribute("s", size);
+        model.addAttribute("mc", motCle);
+
+        return "produitform";
+    }
+
+    @PostMapping("/produitSave")
+    public String sauverProduit(@Valid Produit produit,
+            BindingResult bindingResult, int p, int s, String mc,
+            RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addAttribute("p", p);
+            redirectAttributes.addAttribute("s", s);
+            redirectAttributes.addAttribute("mc", mc);
+            return "produitform";
+        }
+
+        this.repo.save(produit);
+
+        redirectAttributes.addAttribute("p", p);
+        redirectAttributes.addAttribute("s", s);
+        redirectAttributes.addAttribute("mc", mc);
+        return "redirect:/produits";
+    }
+
 }
--- src/main/java/fr/iut/mvcshop/model/Produit.java
+++ src/main/java/fr/iut/mvcshop/model/Produit.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Produit {
@@ -11,8 +14,17 @@ public class Produit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Size(min = 3, max = 30)
     private String nom;
+
+    @NotNull
+    @DecimalMin("0.01")
     private double prix;
+
+    @NotNull
+    @DecimalMin("0")
     private int quantite;
 
     public Produit() {
